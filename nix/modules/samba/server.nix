98743c91a6c68a6427fae33848e98015d8fc256e
Terrance Kennedy <terrance@missingtoken.net>
2026-07-01 11:57:22 -0600

    Fix mode of /storage/nas/k8s

--- nix/modules/samba/server.nix
+++ nix/modules/samba/server.nix
@@ -38,7 +38,7 @@
 
   # Pre-create share root directory used by smb clients.
   systemd.tmpfiles.rules = [
-    "d /storage/nas/k8s 0750 root root -"
+    "d /storage/nas/k8s 0755 root root -"
     "d /storage/nas/backups/longhorn 0750 smb-longhorn smb-longhorn -"
   ];
 
